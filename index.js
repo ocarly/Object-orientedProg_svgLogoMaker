cfcc94f4862f2515abfec89ce31027a3a00bd200
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-10-29 18:28:28 -0500

    added someQuestions.js helps me stay organized.

--- index.js
+++ index.js
@@ -1,74 +1,21 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const questions = require
 const { Circle, Square, Triangle } = require("./lib/shapes");
 const path = require("path");
-const questions = [
-{
-    type: 'input',
-    name: 'text',
-    message: 'Please input 3 letters',
-},
-{
-    type: 'input',
-    name: 'textColor',
-    message: 'What color would you like the text to be?',
-},
-{
-    type: 'list',
-    name: 'shape',
-    message: 'Shape of logo?',
-    choices: ['Circle', 'Triangle', 'Square'],
-},
-{
-    type: 'list',
-    name: 'shapeColor',
-    message: 'Color of logo? Hex also works.',
-},
-]
-inquirer.prompt(questions)
-.then((response) => {
-    const text = response.text;
-    const textColor = response.textColor;
-    const mcShape = response.mcShape;
-    const shapeColor = response.shapeColor;
+const SVG = require("./lib/svg");
+const { sourceMapsEnabled } = require("process");
 
-         makeShapes(text,textColor,mcShape,shapeColor);
-})
-.catch((err) => console.log(err));
+function svcLogo(response) {
 
+}
 
-function makeShapes(text, shapeColor, textColor, mcShape){
-    if (mcShape === 'Circle') {
-        const circle = new Circle(text, textColor, shapeColor)
-        return fs.writeFile('logo.svg',circle.render(),(err) => {
-            if(err) {
-                console.log(err);
-            }
-            else {
-                console.log("logo.svg has been created.");
-            }
-        });   
-    }
-}
-if (mcShape === 'Triangle') {
-    const triangle = new Triangle(text, textColor, shapeColor)
-    return fs.writeFile('logo.svg',triangle.render(),(err) => {
-        if(err) {
-            console.log(err);
-        }
-        else {
-            console.log("logo.svg has been created.");
-        }
-    });   
-}
-if (mcShape === 'Square') {
-    const square = new Square(text, textColor, shapeColor)
-    return fs.writeFile('logo.svg',square.render(),(err) => {
-        if(err) {
-            console.log(err);
-        }
-        else {
-            console.log("logo.svg has been created.");
-        }
-    });   
-}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => console.log(err))};
+
+    init();
