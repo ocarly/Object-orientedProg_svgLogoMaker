e9f73354a5be2d534f8f818041c8ee0c5671aa45
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-10-29 11:58:10 -0500

    added gitignore and organized folders

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const SVG = require("./utils/svg");
+const { Circle, Square, Triangle } = require("./utils/shapes");
