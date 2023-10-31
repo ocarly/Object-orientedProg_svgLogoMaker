634788ffd8899d5ea9822f1f32788c3dd5da0380
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-10-30 22:03:27 -0500

    created function for questions

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const questions = require
+const questions = require("./lib/someQuestions")
 const { Circle, Square, Triangle } = require("./lib/shapes");
 const path = require("path");
 const SVG = require("./lib/svg");
-const { sourceMapsEnabled } = require("process");
 
-function svcLogo(response) {
 
-}
+
 
 function init() {
     inquirer
     .prompt(questions)
     .then((response) => {
-        createLogo(response);
+        console.log(response)
+        let triCirSqu 
+        if(response.shape === 'Square'){
+            triCirSqu = new Square()
+        }else if(response.shape === 'Triangle'){
+            triCirSqu = new Triangle()
+        }else if(response.shape === 'Circle'){
+            triCirSqu = new Circle()
+        }
+        triCirSqu.setShapeColor(response.shapeColor)
+        const finalLogo = new SVG()
+        finalLogo.setShape(triCirSqu)
+        finalLogo.setText(response.text, response.textColor)
         })
     .catch(err => console.log(err))};
 
